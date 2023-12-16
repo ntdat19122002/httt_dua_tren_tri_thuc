dd723b65d486b8d29131abfede6db82ce500dd69
ntdat <ntdat19122002@gmail.com>
2023-12-16 11:40:51 +0700

    final

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import smtplib
+import textwrap
+
 from flask import Flask, render_template, request
 from flask_cors import CORS
 from class_all import *
@@ -5,6 +8,9 @@ from class_all_ptit import ConvertData
 
 from backward_chaining import BackwardChaining
 from forward_chaining import ForwardChaining
+
+from email.message import EmailMessage
+import ssl, smtplib
 
 validate = Validate()
 
@@ -18,19 +24,24 @@ db.getfc()
 db.getbc()
 luat_lui = db.groupbc()
 luat_tien = db.groupfc()
-@app.route("/", methods = ['POST'])
+
+
+@app.route("/", methods=['POST'])
 def hello_world():
     return {
         'reply': 'test thu xem con bot tra loi the nào b'
     }
 
+
 @app.route("/question1")
 def question1():
-    return tra_ve_tt_cho_fe(['S07','S17','S13','S22','S18'],'addTt','reply1')
+    return tra_ve_tt_cho_fe(['S07', 'S17', 'S13', 'S22', 'S18'], 'addTt', 'reply1')
+
 
 @app.route("/question2")
 def question2():
-    return tra_ve_tt_cho_fe(['S08','S16','S12','S21','S17'],'addTt2','reply2')
+    return tra_ve_tt_cho_fe(['S08','S10','S03', 'S05', 'S16', 'S12', 'S21', 'S17'], 'addTt2', 'reply2')
+
 
 @app.route("/forward_chaining", methods=['POST'])
 def tien_hanh_forward_chaining():
@@ -40,11 +51,71 @@ def tien_hanh_forward_chaining():
         benh_du_doan_list.append(temp)
     return benh_du_doan_list
 
+
 @app.route("/backward_chaining", methods=['POST'])
-def tien_hanh_backward_chaining():
-    benh_du_doan_list = backward_chaining(luat_lui,request.json['tt'],request.json['list_predicted_disease'],"ex")
+def tien_hanh_backwarddf_chaining():
+    benh_du_doan_list = backward_chaining(luat_lui, request.json['tt'], request.json['list_predicted_disease'], "ex")
     print(benh_du_doan_list)
     return benh_du_doan_list
+
+@app.route('/wtf',methods=['POST'])
+def ok():
+    return {}
+
+@app.route("/send_email", methods=['POST'])
+def send_email():
+    email_sender = 'anhcv10@gmail.com'
+    email_password = 'bpfmjpsvurfhnmdx'
+    email_receiver = request.json['email']
+    print(email_receiver)
+
+    benh = {}
+    benh = db.get_benh_by_id(request.json['id_benh'])
+    # print(benh)
+    if benh == 0:
+        pass
+    else:
+        nguyen_nhan = benh['nguyennhan']
+        loi_khuyen = benh['loikhuyen']
+        subject = 'Medical records'
+        body = textwrap.dedent(f"""
+Xin chào {request.json['ten']}
+Chúng tôi nhận được các triệu chứng bạn đã gặp phải là :
+{[db.get_trieuchung_by_id(i)["noidung"] for i in request.json['tt']]}
+Chúng tôi dự đoán bạn bị bệnh : {benh['tenBenh']}
+Nguyên nhân gây ra bệnh là:
+{nguyen_nhan}
+Lời khuyên của chúng tôi dành cho bạn:
+{loi_khuyen}
+Rất vui khi được giúp đỡ bạn
+            """)
+        # print(body)
+
+        em = EmailMessage()
+        em['From'] = email_sender
+        em['To'] = email_receiver
+        em['Subject'] = subject
+        em.set_content(body)
+        context = ssl.create_default_context()
+
+        try:
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=ssl.create_default_context()) as smtp:
+                smtp.login(email_sender, email_password)
+
+                # Encode the email body using UTF-8
+                body_utf8 = body.encode('utf-8')
+
+                msg = EmailMessage()
+                msg.set_content(body_utf8.decode('utf-8'))  # Decode for setting the content
+                msg['From'] = email_sender
+                msg['To'] = email_receiver
+                msg['Subject'] = subject
+
+                # Send the email with the encoded content
+                smtp.sendmail(email_sender, email_receiver, msg.as_bytes())
+        except Exception as e:  # Use "except" instead of "exption"
+            print(f"An error occurred: {e}")
+    return {}
 
 
 def forward_chaining(rule, fact, goal, file_name):
@@ -62,6 +133,7 @@ def forward_chaining(rule, fact, goal, file_name):
         end=" ")
     return list_predicted_disease
 
+
 def tra_ve_tt_cho_fe(tt_list, func, id):
     data = []
 
@@ -72,6 +144,7 @@ def tra_ve_tt_cho_fe(tt_list, func, id):
             'function': f"{func}(this, '{tt}', '{id}')"
         })
     return data
+
 
 def backward_chaining(luat_lui, list_symptom_of_person_id, list_predicted_disease, file_name):
     predictD = list_predicted_disease
@@ -98,7 +171,10 @@ def backward_chaining(luat_lui, list_symptom_of_person_id, list_predicted_diseas
             D['loikhuyen'] = D['loikhuyen'].replace("/n", "\n")
             print(f"{D['loikhuyen']}")
             print("Cám ơn bạn đã sử dụng chat bot của chúng tôi")
-            return goal, fact_real
+            return {
+                'benh': db.get_benh_by_id(goal),
+                'tt': fact_real
+            }
 
         while (len(all_s_in_D) > 0):
             s = db.get_trieuchung_by_id(all_s_in_D[0])
@@ -132,11 +208,14 @@ def backward_chaining(luat_lui, list_symptom_of_person_id, list_predicted_diseas
             print(f"{D['loikhuyen']}")
             print("Cám ơn bạn đã sử dụng chat bot của chúng tôi")
 
-            return goal, fact_real
-            break
+            return {
+                'benh':db.get_benh_by_id(goal),
+                'tt': fact_real
+            }
     if benh == 0:
         print(f"Bạn không bị bệnh nào cả")
         return None, fact_real
+
 
 if __name__ == '__main__':
     app.run()
--- client/script.js
+++ client/script.js
@@ -6,6 +6,123 @@ const chatContainer = document.querySelector('#chat_container')
 let trieuChung = []
 let trieuChung1 = []
 let trieuChung2 = []
+let id_benh = null
+let ten = ''
+let email = ''
+let statusApp = 'hoiTen'
+
+function cumA1() {
+    init_message('Triệu chứng:\n' +
+        '_ Sốt cao trên 38 độ, ớn lạnh.\n' +
+        '_ Nhức đầu, chóng mặt, mệt mỏi, đau nhức cơ, biếng ăn, cơ thể suy nhược;\n' +
+        '_ Đau họng, viêm họng, ho khan;\n' +
+        '_ Hắt hơi, sổ mũi, nghẹt mũi, khó thở;\n' +
+        '_ Buồn nôn, ói mửa và tiêu chảy.\n' +
+        'Lời khuyên:\n' +
+        '_ Đến gặp bác sĩ sớm nhất có thể để được khám và xét nghiệm chẩn đoán bệnh chính xác nhất\n' +
+        '_ Nếu đang điều trị tại nhà theo chỉ định của bác sĩ, bạn hãy:\n' +
+        '_ Nghỉ ngơi nhiều nhất có thể\n' +
+        '_ Uống thuốc hạ sốt theo chỉ định\n' +
+        '_ Uống nhiều nước, bổ sung đầy đủ chất dinh dưỡng, ăn đồ dễ tiêu hóa, hạn chế uống nước lạnh\n' +
+        '_ Tắm nước ấm, bận quần áo nhẹ, thông thoáng\n' +
+        '_ Dinh dưỡng đầy đủ và cách li tại nhà\n')
+}
+
+function cumA2() {
+    init_message('Triệu chứng:\n' +
+        '_ Sốt trên 38°C.\n' +
+        '_ Cảm thấy rét run, mệt mỏi, choáng váng đầu óc.\n' +
+        '_ Đau ngực, tim đập nhanh.\n' +
+        '_ Đau họng, ho, thường ho khan, ho có đờm.\n' +
+        '_ Chỉ sau nửa ngày, các triệu chứng do A/H5N1 trở nên trầm trọng. Người bệnh có biểu hiện suy hô hấp cấp như khó thở, thở nhanh, da tím tái. Các triệu chứng đi kèm là đau lan tỏa, mệt mỏi đặc biệt là đau đầu, đau nhức cơ, đau toàn thân, ý thức mê man.\n' +
+        'Lời khuyên:\n' +
+        '_ Đến gặp bác sĩ sớm nhất có thể\n' +
+        '_ Nếu đang điều trị tại nhà theo chỉ định của bác sĩ, bạn hãy:\n' +
+        '_ Nghỉ ngơi, thư giãn trong môi trường thoáng khí, tránh những nơi quá nóng hoặc quá lạnh và không nên ở phòng điều hòa\n' +
+        '_ Dinh dưỡng đầy đủ, ăn các đồ dễ tiêu, thực phẩm lỏng, nóng và uống nhiều nước. Tránh ăn đồ lạnh\n' +
+        '_ Sử dụng nước muối loãng để vệ sinh họng 2-3/ngày\n' +
+        '_ Vệ sinh mũi bằng thuốc xịt mũi mỗi ngày\n')
+}
+
+function cumA3() {
+    init_message('Triệu chứng:\n' +
+        '_ Sốt cao 39 – 40°C.\n' +
+        '_ Đau mỏi các khớp xương, buồn nôn, nôn, đau đầu.\n' +
+        '_ Trong một số trường hợp, bệnh nhân có biểu hiện viêm long đường hô hấp trên như: sổ mũi, hắt hơi, đau họng…\n' +
+        '_ Ho, tức ngực và khó thở tăng dần.\n' +
+        '_ Các triệu chứng suy hô hấp như: tím môi, đầu chi, co kéo cơ hô hấp, thở nhanh.\n' +
+        '_ Các biểu hiện nặng, nguy kịch, bao gồm: thiểu niệu hoặc vô niệu, suy tim, phù, đông máu nội quản rải rác, suy gan nặng, hôn mê…\n' +
+        'Lời khuyên:\n' +
+        '_ Bạn nên đến gặp bác sĩ để khám, cách li và xét nghiệm để chẩn đoán bệnh chính xác nhất và không nên tự ý điều trị ở nhà do loại cúm này có độc lực cao\n')
+}
+
+function cumB() {
+    init_message('Giống như cúm A, vi rút cúm B cũng có thể bùng phát gây bệnh theo mùa. Về khác biệt, vi rút cúm B nói chung thay đổi chậm hơn về đặc tính di truyền và kháng nguyên so với cúm A. Vi rút cúm B chỉ gây bệnh ở người và không được phân chia theo loại như cúm A; cũng không gây ra những đợt lây nhiễm lớn.\n' +
+        'Triệu chứng:\n' +
+        '_ Hô hấp: ho, đau hoặc ngứa rát cổ họng, viêm họng, chảy nước mũi, hắt hơi liên tục\n' +
+        'Sốt vừa đến sốt cao (trên 39 độ C, có thể lên tới 41 độ C), ớn lạnh toàn thân, mệt mỏi, chân tay không có lực, hoa mắt, đau đầu, đau nhức cơ, đau khi vận động\n' +
+        'Buồn nôn và nôn, đau bụng, tiêu chảy, chán ăn, khô miệng, đau dạ dày\n' +
+        'Lời khuyên:\n' +
+        '_ Đến gặp bác sĩ sớm nhất có thể\n' +
+        '_ Nếu đang điều trị tại nhà theo chỉ định của bác sĩ, bạn hãy:\n' +
+        '_ Uống thuốc hạ sốt giảm đau, hạ sốt theo chỉ định\n' +
+        '_ Nghỉ ngơi nhiều hơn, giữ không gian sống sạch sẽ, thoáng mát.\n' +
+        '_ Uống nhiều nước, chia nhỏ các bữa ăn trong ngày.\n' +
+        '_ Bổ sung các loại khoáng chất, vitamin\n')
+}
+
+function thuoc() {
+    init_message('Tamiflu (oseltamivir phosphate) sử dụng điều trị triệu chứng cúm do virus gây ra ở người bệnh có triệu chứng ít hơn 2 ngày. Không tự ý sử dụng thuốc trong điều trị cảm lạnh thông thường. Nên bắt đầu dùng thuốc sớm ngay khi có những triệu chứng của bệnh cúm như: sốt, ớn lạnh, đau cơ, chảy nước mũi, nghẹt mũi, đau họng,… \n' +
+        'Relenza (zanamivir): thuốc dạng hít dùng trong phòng ngừa và điều trị cúm. Thuốc có tác dụng làm giảm sự lây lan của virus cúm. Lưu ý, dạng hít chỉ sử dụng trong các trường hợp không có oseltamivir hoặc kháng với oseltamivir, cho bệnh nhân từ 5 tuổi trở lên. Một số tác dụng phụ thường gặp có thể bao gồm: đau đầu, tiêu chảy, buồn nôn, nôn, chóng mặt.\n' +
+        'Các loại thuốc cũ như amantadine và rimantadine được phê duyệt trong phòng ngừa và điều trị cúm A.\n')
+}
+
+function benh() {
+    init_message('Bạn muốn tím hiểu về bệnh gì?', [
+        {
+            message: 'Cúm A - H1N1',
+            function: 'cumA1()'
+        },
+        {
+            message: 'Cúm A - H5N1',
+            function: 'cumA2()'
+        },
+        {
+            message: 'Cúm A - H7N9',
+            function: 'cumA3()'
+        },
+        {
+            message: 'Cúm B',
+            function: 'cumB()'
+        },
+    ])
+}
+
+function noi() {
+    init_message('Tổ hợp Y tế Mediplus. Địa chỉ: 99 P. Tân Mai, Tân Mai, Hoàng Mai, Hà Nội\n' +
+        'Bệnh viện Đa khoa Medlatec. Địa chỉ: Số 99 Trích Sài, Tây Hồ, Hà Nội\n' +
+        'Hệ thống Y tế Thu Cúc TCI.  Địa chỉ: 216 Trần Duy Hưng, Cầu Giấy, Hà Nội\n' +
+        'Phòng khám Đa khoa Medelab. Địa chỉ: 86-88 Nguyễn Lương Bằng, Chợ Dừa, Đống Đa, Hà Nội\n' +
+        'Trung tâm xét nghiệm Y khoa Labhouse. Địa chỉ: Tòa D, Vinaconex 2, KĐT, Kim Văn, Hoàng Mai, Hà Nội\n' +
+        'Bệnh viện Đa khoa Hồng Ngọc. địa chỉ : 55 P. Yên Ninh, Quán Thánh, Ba Đình, Hà Nội')
+}
+
+function traCuu() {
+    init_message('Bạn muốn tìm hiểu gì về bệnh cúm', [
+        {
+            message: 'Thuôc trị bệnh cúm',
+            function: 'thuoc()'
+        },
+        {
+            message: 'Các loại bệnh cúm',
+            function: 'benh()'
+        },
+        {
+            message: 'Nơi điều trị',
+            function: 'noi()'
+        },
+    ])
+}
 
 function addTt(e, tt, id) {
     if (e.classList.contains("picked")) {
@@ -56,7 +173,7 @@ const reply1 = async () => {
         const response = await fetch('http://127.0.0.1:5000/question2')
 
         const data = await response.json();
-        init_message('Bạn bị cúm mất rồi, cùng tìm hiểu để xem cụ thể bạn bị cúm gì nhé!\nBạn cảm thấy các triệu chứng nào sau đây không?',
+        init_message(`${ten} hình như bị cúm mất rồi, cùng tìm hiểu để xem cụ thể bạn bị cúm gì nhé!\nBạn cảm thấy các triệu chứng nào sau đây không?`,
             data,
             [{
                 id: 'reply2',
@@ -81,13 +198,13 @@ const reply2 = async () => {
 
     const data = await response.json();
     let message = ''
-    if (data.length > 1){
+    if (data.length > 1) {
         message += 'Dự đoan bạn có thể mắc 1 trong cái loại bệnh cúm sau: '
-        for (let benh of data){
+        for (let benh of data) {
             message += benh['tenBenh'] + ','
         }
         let list_predicted_disease = []
-        for(let i of data){
+        for (let i of data) {
             list_predicted_disease.push(i['idbenh'])
         }
         const responseBw = await fetch('http://127.0.0.1:5000/backward_chaining', {
@@ -100,12 +217,20 @@ const reply2 = async () => {
                 list_predicted_disease: list_predicted_disease
             })
         })
+        let benh_chuan = await responseBw.json()
+        message += `. Khả năng cao hơn bạn sẽ mắc bênh ${benh_chuan['benh']['tenBenh']}. Bạn nên làm theo lời khuyên sau: \n ${benh_chuan['benh']['loikhuyen']}
+            Bạn có muốn nhận thêm thông tin về email, hãy nhập email của bạn.`
+        statusApp = 'guiEmail'
+        id_benh = benh_chuan['benh']['idbenh']
         init_message(message)
-        console.log(responseBw.json())
-    }else if (data.length==1){
-        init_message('Bạn bị mắc bệnh cúm ' + data[0]['tenBenh'])
-    }else{
-        init_message('Chưa có đủ dữ liệu. Bản trả lời thêm các câu hỏi nhé')
+        console.log(benh_chuan)
+    } else if (data.length == 1) {
+        init_message('Bạn bị mắc bệnh cúm ' + data[0]['tenBenh'] + '. Bạn nên làm theo lời khuyên sau: \n' + data[0]['loikhuyen']
+            + 'Bạn có muốn nhận thêm thông tin về email, hãy nhập email của bạn.')
+        statusApp = 'guiEmail'
+        id_benh = data[0]['idbenh']
+    } else {
+        init_message('Vậy bạn không có bị cúm đâu, chỉ hơi sốt chút xíu thôi.')
     }
 }
 const timBenh = async () => {
@@ -113,7 +238,7 @@ const timBenh = async () => {
 
     const data = await response.json();
 
-    init_message('Bạn có triệu trứng nào sau đây không?',
+    init_message(`${ten}có triệu trứng nào sau đây không?`,
         data,
         [{
             id: 'reply1',
@@ -173,7 +298,7 @@ let loadInterval
 
 const init_message = async (message, options, replies) => {
     const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId, options)
 
     // to focus scroll to the bottom
     chatContainer.scrollTop = chatContainer.scrollHeight;
@@ -184,15 +309,7 @@ const init_message = async (message, options, replies) => {
     await typeText(messageDiv, message, options, replies)
 }
 
-init_message('Chào mừng bạn tới với chatbot tư vấn bệnh cúm ? Không biết tôi có thể giúp gì cho bạn ?',
-    [{
-        message: 'Tôi nghi ngờ mình mắc bệnh cúm',
-        function: 'timBenh()'
-    }, {
-        message: 'Tra cứu thông tin bệnh cúm',
-        function: ''
-    }])
-
+init_message("Có thể cho tôi biết tên bạn được không ?")
 
 function loader(element) {
     element.textContent = ''
@@ -216,7 +333,11 @@ function generateUniqueId() {
     return `id-${timestamp}-${hexadecimalString}`;
 }
 
-function chatStripe(isAi, value, uniqueId) {
+function chatStripe(isAi, value, uniqueId, option) {
+    let a = ''
+    if (!option) {
+        a = 'wrap'
+    }
     return (
         `
         <div class="wrapper ${isAi && 'ai'}">
@@ -227,7 +348,7 @@ function chatStripe(isAi, value, uniqueId) {
                       alt="${isAi ? 'bot' : 'user'}" 
                     />
                 </div>
-                <div class="message" id=${uniqueId}>${value}</div>
+                <div class="message ${a}" id=${uniqueId}>${value}</div>
             </div>
         </div>
     `
@@ -246,42 +367,67 @@ const handleSubmit = async (e) => {
     form.reset()
 
     // bot's chatstripe
-    const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+    // const uniqueId = generateUniqueId()
+    // chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
     // to focus scroll to the bottom
     chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    // specific message div
-    const messageDiv = document.getElementById(uniqueId)
-
-    // messageDiv.innerHTML = "..."
-    loader(messageDiv)
-
-    const response = await fetch('http://127.0.0.1:5000 ', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt: data.get('prompt')
+    if (statusApp == 'hoiTen') {
+        ten = data.get('prompt').trim()
+        init_message(`Chào mừng ${ten} tới với chatbot tư vấn bệnh cúm ? Không biết tôi có thể giúp gì cho bạn ?`,
+            [{
+                message: 'Tôi nghi ngờ mình mắc bệnh cúm',
+                function: 'timBenh()'
+            }, {
+                message: 'Tra cứu thông tin bệnh cúm',
+                function: 'traCuu()'
+            }])
+    } else if (statusApp == 'guiEmail') {
+        const response = await fetch('http://127.0.0.1:5000/send_email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: data.get('prompt'),
+                ten: ten,
+                id_benh: id_benh,
+                tt: trieuChung
+            })
         })
-    })
-
-    clearInterval(loadInterval)
-    messageDiv.innerHTML = " "
-
-    if (response.ok) {
-        const data = await response.json();
-        const parsedData = data.reply.trim() // trims any trailing spaces/'\n'
-
-        typeText(messageDiv, parsedData)
-    } else {
-        const err = await response.text()
-
-        messageDiv.innerHTML = "Something went wrong"
-        alert(err)
+        init_message('Tôi đã gửi thông tin về email bạn')
     }
+    // specific message div
+    // const messageDiv = document.getElementById(uniqueId)
+    //
+    // // messageDiv.innerHTML = "..."
+    // loader(messageDiv)
+    //
+    // const response = await fetch('http://127.0.0.1:5000 ', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //         prompt: data.get('prompt')
+    //     })
+    // })
+    //
+    // clearInterval(loadInterval)
+    // messageDiv.innerHTML = " "
+    //
+    // if (response.ok) {
+    //     const data = await response.json();
+    //     const parsedData = data.reply.trim() // trims any trailing spaces/'\n'
+    //
+    //     typeText(messageDiv, parsedData)
+    // } else {
+    //     const err = await response.text()
+    //
+    //     messageDiv.innerHTML = "Something went wrong"
+    //     alert(err)
+    // }
 }
 
 form.addEventListener('submit', handleSubmit)
